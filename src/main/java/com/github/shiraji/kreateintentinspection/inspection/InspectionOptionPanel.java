d6b8b0da34cfb1deea83e59cf3d740774a265c2f
shiraji <isogai.shiraji@gmail.com>
2016-06-14 06:55:16 +0900

    Create option panel

---
+++ src/main/java/com/github/shiraji/kreateintentinspection/inspection/InspectionOptionPanel.java
@@ -0,0 +1,8 @@
+package com.github.shiraji.kreateintentinspection.inspection;
+
+import javax.swing.*;
+
+public class InspectionOptionPanel {
+    JPanel panel;
+    JTextField methodNameTextField;
+}
